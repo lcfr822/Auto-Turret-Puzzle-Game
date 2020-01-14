74f9ee7e2fa514b768ed1da76697a9d9a9b099dc
lcfr822 <lcfr822@gmail.com>
2020-01-14 02:26:12 -0800

    Added: prototype turret template Added: foundation of turret camera tracking Added: target identification class Issue: turrets perpetually gain and lose lock when a target is visible.

---
+++ Scripts/Turret/BaseTurret.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class BaseTurret : MonoBehaviour
+{
+    [SerializeField]
+    private Transform turretPivot, turretBarrel, turretReceiver;
+    public List<GameObject> trackedTargets = new List<GameObject>();
+    public Camera turretTrackingCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ReportTarget(GameObject newTarget)
+    {
+        Debug.Log("Repeat? " + trackedTargets.Contains(newTarget));
+        if (trackedTargets.Contains(newTarget)) { return; }
+        Debug.LogWarning("Logging new target: " + newTarget.name);
+        trackedTargets.Add(newTarget);
+        trackedTargets.OrderBy(t => Vector3.Distance(transform.position, t.transform.position));
+
+        int targetIndex = 0;
+        foreach(GameObject target in trackedTargets)
+        {
+            Debug.Log(targetIndex + ". " + Vector3.Distance(transform.position, target.transform.position));
+            targetIndex++;
+        }
+    }
+
+    public void ReleaseTarget(GameObject lostTarget)
+    {
+        trackedTargets.Remove(lostTarget);
+        trackedTargets.OrderBy(t => Vector3.Distance(transform.position, t.transform.position));
+
+        int targetIndex = 0;
+        foreach (GameObject target in trackedTargets)
+        {
+            Debug.Log(targetIndex + ". " + Vector3.Distance(transform.position, target.transform.position));
+            targetIndex++;
+        }
+    }
+
+    public void TranslateToTarget()
+    {
+
+    }
+}
---
+++ Scripts/Turret/TargetObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetObject : MonoBehaviour
+{
+    public Camera testCamera;
+    private BaseTurret alertedTurret;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (GetComponent<Renderer>().IsVisibleFrom(testCamera) && !FindObjectOfType<BaseTurret>().trackedTargets.Contains(gameObject))
+        {
+            FindObjectOfType<BaseTurret>().ReportTarget(gameObject);
+            alertedTurret = FindObjectOfType<BaseTurret>();
+        }
+        else
+        {
+            alertedTurret.ReleaseTarget(gameObject);
+            alertedTurret = null;
+        }
+    }
+}
